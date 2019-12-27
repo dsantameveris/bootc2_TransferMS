5427fbf54e92e18bbf4b38cb2eb9d93e482b1260
Jaeggler <gonzalo.santacruzm@gmail.com>
2019-12-27 15:48:10 -0500

    Transfer Microservice creation

---
+++ src/main/java/com/everis/TransferMS/TransferMsApplication.java
@@ -0,0 +1,13 @@
+package com.everis.TransferMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/TransferMS/TransferMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.TransferMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
